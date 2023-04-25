752fe61281a483313ebfd5bc5f4057bf5d05274b
George Surmava <111701072+surmavagit@users.noreply.github.com>
2023-04-25 18:23:10 +0200

    The frontend refreshes with the server state. The buttons change the server state.
    Co-authored-by: surmavagit <111701072+surmavagit@users.noreply.github.com>
    Co-authored-by: denigogov <122295314+denigogov@users.noreply.github.com>

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,59 @@
+import axios from "axios";
+import useSWR, { useSWRConfig } from "swr";
+
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+
 function Login() {
-    // TODO
-    return <h1>Hello, Vite</h1>
+  const { data, error, isLoading } = useSWR("http://localhost:4000/", fetcher);
+  const { mutate } = useSWRConfig();
+
+  if (error) return <h3>{error.message} </h3>;
+  if (isLoading) return <h3>loading...</h3>;
+
+
+
+
+  // render data
+  return data.loggedin ? (
+    <>
+      <h2>Welcome, UniversalUser!</h2>
+      <button
+        onClick={() => {
+          axios
+            .post("http://localhost:4000/logout")
+            .then(function (response) {
+              console.log(response);
+            })
+            .catch(function (error) {
+              console.error(error);
+            });
+          mutate("http://localhost:4000/");
+        }}
+      >
+        Logout
+      </button>
+    </>
+  ) : (
+    <>
+      <h1>Please, login</h1>
+
+      <button
+        onClick={() => {
+          axios
+            .post("http://localhost:4000/login")
+            .then(function (response) {
+              console.log(response);
+            })
+            .catch(function (error) {
+              console.error(error);
+            });
+          mutate("http://localhost:4000/");
+        }}
+      >
+        Login
+      </button>
+    </>
+  );
 }
 
 export default Login;
